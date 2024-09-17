314a5d3bf8cd2bbd825a8fe940e393ca9d9c5c1a
Deftly <deftkz@gmail.com>
2024-09-16 18:55:58 -0700

    additional placeholder pages

--- leapp_reporting_client/src/App.tsx
+++ leapp_reporting_client/src/App.tsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "@/pages/AppLayout";
 import UnderConstruction from "@/pages/UnderConstruction";
+import Analytics from "@/pages/Analytics";
 import Workflows from "@/pages/Workflows";
+import Dashboard from "@/pages/Dashboard";
+import Hosts from "@/pages/Hosts";
 import Stages from "@/pages/Stages";
 
 const App = () => {
@@ -9,15 +12,12 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route element={<AppLayout />}>
-          <Route index element={<UnderConstruction page="Dashboard" />} />
+          <Route index element={<Dashboard />} />
           <Route path="workflows" element={<Workflows />} />
           <Route path="workflows/:hostname/:txId/stages" element={<Stages />} />
-          <Route path="hosts" element={<UnderConstruction page="Hosts" />} />
-          <Route
-            path="analytics"
-            element={<UnderConstruction page="Analytics" />}
-          />
-          <Route path="FAQ" element={<UnderConstruction page="FAQ" />} />
+          <Route path="hosts" element={<Hosts />} />
+          <Route path="analytics" element={<Analytics />} />
+          <Route path="FAQ" element={<UnderConstruction />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ leapp_reporting_client/src/pages/Hosts.tsx
@@ -0,0 +1,5 @@
+const Hosts = () => {
+  return <div>Hosts</div>;
+};
+
+export default Hosts;
